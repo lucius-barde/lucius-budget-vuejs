abf4d234a9aa20fdb2440ce825db49be0b6513d4
Lucien <l.piccand@protonmail.ch>
2026-09-23 13:55:10 +0200

    footer fix

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar.vue'
   <div class="min-h-screen flex flex-col">
     <NavBar />
     <RouterView />
-    <footer class="text-center m-4 p-4 text-gray-600 text-xs">
+    <footer class="text-center p-2 text-white text-xs">
         &copy; 2026 <a href="https://www.bardic.space" target="_blank" class="underline">Bardic.Space</a>
     </footer>
   </div>
